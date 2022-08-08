61233d4915b4bd37a8211091fe0d6f42e344d80d
Aritra Das <aritrarick2002@gmail.com>
2022-08-08 23:43:12 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/geekymuskeeters/keepit/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.geekymuskeeters.keepit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
